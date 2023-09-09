5cdf616df4236b67095527b2b7acfe15430cc8f2
apple <apple@appledeMacBook-Air.local>
2023-09-09 12:34:11 +0800

    fix

--- src/views/home/details.vue
+++ src/views/home/details.vue
@@ -120,7 +120,7 @@ export default {
         console.log('this.$route', this.$route)
         this.goodType = this.$route.name
         if (this.$route.name === 'good') {
-            this.matchNFTData(parseInt(this.$route.params.tokenId))
+            this.matchNFTData(parseInt(this.$route.params.id))
         } else if (this.$route.name === 'market') {
             this.tokenId = this.$route.params.tokenId
             this.getNFTDetails()
