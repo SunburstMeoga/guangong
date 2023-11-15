373066cea91e16539d025279d2cb747f863a2d53
apple <apple@appledeMacBook-Air.local>
2023-11-15 17:14:56 +0800

    fix:get campaign earning toast

--- src/views/personal/campaignEarning.vue
+++ src/views/personal/campaignEarning.vue
@@ -139,7 +139,7 @@ export default {
                 console.log('canCampaignAgain', Number(item.utc) + cycle_num)
                 if (!canCampaignAgain) {
 
-                    showToast(`请在${this.resultFormat(Number(item.utc) + cycle_num - timeStamp)}后再次出征`)
+                    showToast(`请在${this.resultFormat(Number(item.utc) + cycle_num - timeStamp)}领取收益`)
                     return
                 }
             }
